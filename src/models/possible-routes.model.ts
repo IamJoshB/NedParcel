002733b1559062df5c2a230f25e778e487e8e7e2
IamJoshB <48671399+IamJoshB@users.noreply.github.com>
2025-10-29 12:05:55 +0200

    Enhance routesSchema with new fields
    Added price, driverSplit, and associationSplit fields to routesSchema.

--- src/models/possible-routes.model.ts
+++ src/models/possible-routes.model.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const routesSchema = new mongoose.Schema(
   {
     farePrice: { type: Number, required: true },
+    price: { type: Number, required: true }, // package movement price between the two ranks
+    driverSplit: { type: Number, required: true, default: 0 },
+    associationSplit: { type: Number, required: true, default: 0 },
     distance: { type: Number, required: true },
     fromRank: {
       type: mongoose.Schema.Types.ObjectId,
